20b503fde86b05037d85e95d82e071fa690a8e88
Saurav Kothari <sauravkothari2@gmail.com>
2018-04-09 18:13:08 +0530

    Timeline skeleton

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,8 +1,16 @@
 var links = document.getElementsByClassName("nav-action");
 var sections = document.getElementsByClassName("section-content");
+var timelinePoints = document.getElementsByClassName("timeline-point");
+var timelineLine = document.getElementById("timeline-line");
 
 for(var i = 0; i < links.length; i++) {
 	links[i].addEventListener("click", show_section, false);
+}
+
+
+for(var i = 0; i < timelinePoints.length; i++) {
+	timelinePoints[i].style.left =  parseInt(timelineLine.offsetWidth) * parseInt(timelinePoints[i].dataset.location) / 100 + "px"
+	timelinePoints[i].addEventListener("mouseover", show_timeline_text, false)
 }
 
 function show_section(e) {
@@ -19,4 +27,12 @@ function show_section(e) {
 		links[i].classList.remove("selected");
 	}
 	this.classList.add("selected");
+}
+
+function show_timeline_text(e) {
+	text_to_show = this.dataset.text;
+	var text_element = document.getElementById("visibile-text");
+	text_element.classList.remove("hide");
+	text_element.innerHTML =  text_to_show;
+	text_element.style.left = this.style.left;
 }
